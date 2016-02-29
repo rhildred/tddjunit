37debd09981decd8eaadc221e2f7c2f86b1fd618
Rich Hildred <rhildred@gmail.com>
2016-02-28 20:51:01 -0500

    initial commit

---
+++ src/main/java/io/github/rhildred/tddjunit/HSTCalculate.java
@@ -0,0 +1,10 @@
+package io.github.rhildred.tddjunit;
+
+/**
+ * Created by rhildred on 2016-02-28.
+ */
+public class HSTCalculate {
+    public static float calculate(float nIn, String sProv){
+        return 0;
+    }
+}
